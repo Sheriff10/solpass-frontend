2e63c0f4651686549de3637f61562f9cc3a9fcaf
Ibrahim Sheriff <ibrahimsheriff999@gmail.com>
2024-10-08 22:23:34 +0100

    edit usecase section

--- src/pages/homepage/Nftplatform.jsx
+++ src/pages/homepage/Nftplatform.jsx
@@ -24,7 +24,7 @@ function SolPassUses() {
   ];
 
   return (
-    <section className=" bg-black text-white">
+    <section className="text-white">
       <div className="container">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
           {services.map((service) => (
